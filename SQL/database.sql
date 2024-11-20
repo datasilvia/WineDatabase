bd105dab2971e55775a4cbe5a59277d2ffd2ffed
Gema <132750075+GemaVNZ@users.noreply.github.com>
2024-11-20 09:24:53 +0100

    Se reestructura el proyecto y se añaden más cosas.

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,32 +1,32 @@
--- Agregar clave primaria a la tabla 'wines'
+-- 1. Agregar clave primaria a la tabla 'wines'
 ALTER TABLE wines
 ADD PRIMARY KEY (id);
 
--- Agregar clave primaria a la tabla 'winery'
+-- 2. Agregar clave primaria a la tabla 'winerys'
 ALTER TABLE winerys
 ADD PRIMARY KEY (winery_id);
 
--- Agregar clave foránea a 'winery' (relación con 'wines')
+-- 3. Relacionar la tabla 'winerys' con 'wines' mediante una clave foránea
 ALTER TABLE winerys
 ADD CONSTRAINT fk_winery_wine
 FOREIGN KEY (wine_id) REFERENCES wines(id)
 ON DELETE CASCADE;
 
--- Agregar clave foránea a 'winery' (relación con 'region')
+-- 4. Relacionar la tabla 'winerys' con 'regions' mediante una clave foránea
 ALTER TABLE winerys
 ADD CONSTRAINT fk_winery_region
-FOREIGN KEY (region_id) REFERENCES region(region_id)
+FOREIGN KEY (region_id) REFERENCES regions(region_id)
 ON DELETE SET NULL;
 
--- Agregar clave primaria a la tabla 'region'
+-- 5. Agregar clave primaria a la tabla 'regions'
 ALTER TABLE regions
 ADD PRIMARY KEY (region_id);
 
--- Agregar clave primaria a la tabla 'type'
+-- 6. Agregar clave primaria a la tabla 'designations'
 ALTER TABLE designations
 ADD PRIMARY KEY (type_id);
 
--- Agregar clave foránea a 'type' (relación con 'wines')
+-- 7. Relacionar la tabla 'designations' con 'wines' mediante una clave foránea
 ALTER TABLE designations
 ADD CONSTRAINT fk_type_wine
 FOREIGN KEY (wine_id) REFERENCES wines(id)
